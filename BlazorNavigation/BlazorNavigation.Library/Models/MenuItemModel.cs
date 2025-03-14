0b88c27e725fa781596cfe2dd2a5783d220b23b1
Dimitar Gaydardzhiev <dimiter.gg@gmail.com>
2025-03-14 16:38:08 +0100

    implementation

---
+++ BlazorNavigation/BlazorNavigation.Library/Models/MenuItemModel.cs
@@ -0,0 +1,9 @@
+﻿namespace BlazorNavigation.Library.Models
+{
+    internal class MenuItemModel
+    {
+        public string Text { get; set; } = string.Empty;
+
+        public List<MenuItemModel>? SubItems { get; set; }
+    }
+}
